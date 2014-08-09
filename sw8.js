ddc76ebbd3b62800c94b8011ff900a8205d48a5a
Brian <punzalan25brian@gmail.com>
2014-08-09 14:49:29 +0800

    sw8

--- sw8.js
+++ sw8.js
@@ -1,7 +1,14 @@
 var joinArray = function(inputArray, seperator){
+	var text = "";
 	for(var i=0; i<inputArray.length; i++){
-		
+		if(i === inputArray.length-1){
+			text = text.concat(inputArray[i]);
+		}
+		else{
+			text = text.concat(inputArray[i])+seperator;	
+		}
 	}
+	console.log(text);
 }
 
 var myColor = ["Red", "Green", "White", "Black"];
