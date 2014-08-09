27869e18a2d94a0e76299ab478ea1d0d77e7a31b
Brian <punzalan25brian@gmail.com>
2014-08-09 14:23:21 +0800

    sw10 -changed

--- sw10.js
+++ sw10.js
@@ -1,4 +1,8 @@
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+String.prototype.capitalize=function(all){
+    if(all){
+       return this.split(' ').map(function(e){return e.capitalize();}).join(' ');    
+    }else{
+         return this.charAt(0).toUpperCase() + this.slice(1);
+    } 
 }
-"hello world".capitalize();
+"bahay kubo kahit munti".capitalize(true);
